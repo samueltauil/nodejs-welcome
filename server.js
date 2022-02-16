c9b9206564d73ef8bdfbedaa02b13156468c8434
Samuel Tauil <samueltauil@gmail.com>
2022-02-16 15:54:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var healthy=true;
 
 
 app.get('/', function (req, res) {
-  res.send('Welcome App [version: 1 - ' + os.hostname() + ']\n');
+  res.send('Welcome App [version: 2 - ' + os.hostname() + ']\n');
 });
 
 
